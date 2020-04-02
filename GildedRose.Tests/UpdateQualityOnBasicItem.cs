c817f504310463eed6de70205d661b6d2ab91dcf
ViliusVZ <46137434+ViliusVZ@users.noreply.github.com>
2020-04-02 22:33:30 +0300

    Extracted GildedRoseWarehouse used in tests to a private field.

--- GildedRose.Tests/UpdateQualityOnBasicItem.cs
+++ GildedRose.Tests/UpdateQualityOnBasicItem.cs
@@ -4,25 +4,24 @@ using Xunit;
 
 namespace GildedRose.Tests
 {
-	public class UpdateQualityOnBasicItemTests
+	public class UpdateQualityOnBasicItem
 	{
+		private GildedRoseWarehouse _warehouse = new GildedRoseWarehouse();
+
+		public UpdateQualityOnBasicItem() => _warehouse = new GildedRoseWarehouse();
+
 		[Fact]
-		public void RunWithEmptyCollection()
-		{
-			var warehouse = new GildedRoseWarehouse();
-			warehouse.UpdateQuality(new List<Item>());
-		}
+		public void RunWithEmptyCollection() => _warehouse.UpdateQuality(new List<Item>());
 
 		[Fact]
 		public void ReduceQuality()
 		{
-			var warehouse = new GildedRoseWarehouse();
 			var basicItem = new Item() { Name = "Basic item", Quality = 10, SellIn = 5 };
 			var items = new List<Item>() { basicItem };
 
 			var expectedQuality = items[0].Quality - 1;
 
-			warehouse.UpdateQuality(items);
+			_warehouse.UpdateQuality(items);
 
 			Assert.Equal(expectedQuality, basicItem.Quality);
 		}
@@ -30,13 +29,12 @@ namespace GildedRose.Tests
 		[Fact]
 		public void ReduceSellIn()
 		{
-			var warehouse = new GildedRoseWarehouse();
 			var basicItem = new Item() { Name = "Basic item", Quality = 10, SellIn = 5 };
 			var items = new List<Item>() { basicItem };
 
 			var expectedSellIn = items[0].SellIn - 1;
 
-			warehouse.UpdateQuality(items);
+			_warehouse.UpdateQuality(items);
 
 			Assert.Equal(expectedSellIn, basicItem.SellIn);
 		}
@@ -44,14 +42,13 @@ namespace GildedRose.Tests
 		[Fact]
 		public void ReduceQualityByTwoIfSellInIsZero()
 		{
-			var warehouse = new GildedRoseWarehouse();
 			var basicItem = new Item() { Name = "Basic item", Quality = 10, SellIn = 0 };
 			var items = new List<Item>() { basicItem };
 
 			var expectedQuality = 8;
 
 			// Should do a -2 to quality because the SellIn is 0.
-			warehouse.UpdateQuality(items);
+			_warehouse.UpdateQuality(items);
 
 			Assert.Equal(expectedQuality, basicItem.Quality);
 		}
