3705d8c3d88f9affb80f4250f27232a62702359e
ViliusVZ <46137434+ViliusVZ@users.noreply.github.com>
2020-04-02 23:47:49 +0300

    Refactored item names in UpdateQuality.cs

--- GildedRose.Console/GildedRoseWarehouse.cs
+++ GildedRose.Console/GildedRoseWarehouse.cs
@@ -8,15 +8,15 @@ namespace GildedRose.ConsoleApp
 		{
 			for (var i = 0; i < items.Count; i++)
 			{
-				if (items[i].Name != "Aged Brie" && items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+				if (items[i].Name != ItemNameDictionary.AgedBrieName && items[i].Name != ItemNameDictionary.BackstagePassName)
 				{
 					if (items[i].Quality > 0)
 					{
-						if (items[i].Name != "Sulfuras, Hand of Ragnaros")
+						if (items[i].Name != ItemNameDictionary.SulfurasName)
 						{
 							items[i].Quality = items[i].Quality - 1;
 
-							if (items[i].Name.StartsWith("Conjured") && items[i].Quality > 0) items[i].Quality -= 1;
+							if (items[i].Name.StartsWith(ItemNameDictionary.ConjuredItemPrefix) && items[i].Quality > 0) items[i].Quality -= 1;
 						}
 					}
 				}
@@ -26,7 +26,7 @@ namespace GildedRose.ConsoleApp
 					{
 						items[i].Quality = items[i].Quality + 1;
 
-						if (items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+						if (items[i].Name == ItemNameDictionary.BackstagePassName)
 						{
 							if (items[i].SellIn < 11)
 							{
@@ -47,24 +47,24 @@ namespace GildedRose.ConsoleApp
 					}
 				}
 
-				if (items[i].Name != "Sulfuras, Hand of Ragnaros")
+				if (items[i].Name != ItemNameDictionary.SulfurasName)
 				{
 					items[i].SellIn = items[i].SellIn - 1;
 				}
 
 				if (items[i].SellIn < 0)
 				{
-					if (items[i].Name != "Aged Brie")
+					if (items[i].Name != ItemNameDictionary.AgedBrieName)
 					{
-						if (items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+						if (items[i].Name != ItemNameDictionary.BackstagePassName)
 						{
 							if (items[i].Quality > 0)
 							{
-								if (items[i].Name != "Sulfuras, Hand of Ragnaros")
+								if (items[i].Name != ItemNameDictionary.SulfurasName)
 								{
 									items[i].Quality = items[i].Quality - 1;
 
-									if (items[i].Name.StartsWith("Conjured") && items[i].Quality > 0) items[i].Quality -= 1;
+									if (items[i].Name.StartsWith(ItemNameDictionary.ConjuredItemPrefix) && items[i].Quality > 0) items[i].Quality -= 1;
 								}
 							}
 						}
